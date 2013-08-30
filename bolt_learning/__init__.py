2b291896d43de587a6080abd9e7a1116825d3a3e
Ubuntu <ubuntu@ip-172-31-16-240.us-west-2.compute.internal>
2013-08-30 21:21:32 +0000

    new files

---
+++ bolt_learning/__init__.py
@@ -0,0 +1,2 @@
+# Import all of the constants so that they can be directly used
+from constants import *
---
+++ bolt_learning/constants.py
@@ -0,0 +1,4 @@
+#This token is lowercase but in the files appears as upper case, we migth switch the code such that
+#it appears as in the files for consistency
+
+DELETE_TOKEN = '*delete*'
