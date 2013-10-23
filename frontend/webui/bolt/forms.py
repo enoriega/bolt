7f1a87ca1701a60d743b31e29b2ba70fb9ce8533
Ubuntu <ubuntu@ip-172-31-16-240.us-west-2.compute.internal>
2013-10-23 06:05:54 +0000

    raised the max number of characters on the retype input text box from 100 to 300 characters

--- frontend/webui/bolt/forms.py
+++ frontend/webui/bolt/forms.py
@@ -2,5 +2,5 @@ from django import forms
 from bootstrap_toolkit.widgets import *
 
 class RetypeForm(forms.Form):
-    sentence = forms.CharField(max_length=100, widget=BootstrapTextInput(prepend='Your input', attrs={'class':'input-xxlarge'})
+    sentence = forms.CharField(max_length=300, widget=BootstrapTextInput(prepend='Your input', attrs={'class':'input-xxlarge'})
     , label='', required=True,)
